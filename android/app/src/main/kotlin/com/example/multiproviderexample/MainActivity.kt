309ed34272d54e096ab52add7b327273f409358b
tameralshazly <tameralshazly@yahoo.com>
2023-07-29 02:12:52 +0400

    step-01

---
+++ android/app/src/main/kotlin/com/example/multiproviderexample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.multiproviderexample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    ),
+  );
+}
+
+String now() => DateTime.now().toIso8601String();
+
+@immutable
+class Seconds {
+  final String value;
+  Seconds() : value = now();
+}
+
+@immutable
+class Minutes {
+  final String value;
+  Minutes() : value = now();
+}
+
+Stream<String> newStream(Duration duration) => Stream.periodic(
+      duration,
+      (_) => now(),
+    );
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: MultiProvider(
+        providers: [
+          StreamProvider.value(
+            value: Stream<Seconds>.periodic(
+              const Duration(seconds: 1),
+              (_) => Seconds(),
+            ),
+            initialData: Seconds(),
+          ),
+          StreamProvider.value(
+            value: Stream<Minutes>.periodic(
+              const Duration(minutes: 1),
+              (_) => Minutes(),
+            ),
+            initialData: Minutes(),
+          ),
+        ],
+        child: const Column(children: [
+          Row(
+            children: [
+              SecondsWidget(),
+              MinutesWidget(),
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class SecondsWidget extends StatelessWidget {
+  const SecondsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final seconds = context.watch<Seconds>();
+    return Expanded(
+      child: Container(
+        color: Colors.yellow,
+        height: 100,
+        child: Text(
+          seconds.value,
+        ),
+      ),
+    );
+  }
+}
+
+class MinutesWidget extends StatelessWidget {
+  const MinutesWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final minutes = context.watch<Minutes>();
+    return Expanded(
+      child: Container(
+        color: Colors.blue,
+        height: 100,
+        child: Text(
+          minutes.value,
+        ),
+      ),
+    );
+  }
+}
